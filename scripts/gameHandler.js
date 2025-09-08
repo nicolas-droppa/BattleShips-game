d55af87f449082a615f0264409c3071a76546a9e
nicolas-droppa <droppa.nicolas@gmail.com>
2025-09-08 21:36:45 +0200

    displaying ships in panel

--- scripts/gameHandler.js
+++ scripts/gameHandler.js
@@ -18,12 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function startGame(difficulty) {
     document.getElementById("menu").style.display = "none";
-    document.getElementById("game").style.display = "block";
+    document.getElementById("game").style.display = "flex";
 
     console.log("Starting game on diff:", difficulty);
 
     createBoard("player-board");
     createBoard("ai-board");
+    createShips();
 }
 
 function createBoard(boardId) {
@@ -35,4 +36,24 @@ function createBoard(boardId) {
         cell.classList.add("cell");
         board.appendChild(cell);
     }
+}
+
+function createShips() {
+    const shipsPanel = document.querySelector(".ships");
+    shipsPanel.innerHTML = "";
+
+    const shipSizes = [5, 4, 3, 3, 2];
+
+    shipSizes.forEach(size => {
+        const ship = document.createElement("div");
+        ship.classList.add("ship");
+
+        for (let i = 0; i < size; i++) {
+            const cell = document.createElement("div");
+            cell.classList.add("ship-cell");
+            ship.appendChild(cell);
+        }
+
+        shipsPanel.appendChild(ship);
+    });
 }
